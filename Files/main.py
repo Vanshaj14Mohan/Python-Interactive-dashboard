46b6e8203d0abf64ce993813e6aebf56ab3e45a6
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-05-16 23:57:03 +0530

    Files

--- Files/main.py
+++ Files/main.py
@@ -108,10 +108,11 @@ with cl1:
 
 with cl2:
     with st.expander("Region_ViewData"):
-        region = filtered_df.groupby(by = ["Region"], as_index= False)["Sales"].sum()
-        st.write(category_df.style.background_gradient(cmap="Blues"))
-        csv = category_df.to_csv(index = False).encode("utf-8")
-        st.download_button("Download the Data", data = csv, file_name = "Category.csv", mime = "text/csv", help = "Click here to download data as a CSV file")
+        # region = filtered_df.groupby(by = ["Region"], as_index= False)["Sales"].sum()
+        region = filtered_df.groupby(by = "Region", as_index= False)["Sales"].sum()
+        st.write(region.style.background_gradient(cmap="Oranges"))
+        csv = region.to_csv(index = False).encode("utf-8")
+        st.download_button("Download the Data", data = csv, file_name = "Region.csv", mime = "text/csv", help = "Click here to download data as a CSV file")
 
 
 
